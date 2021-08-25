81eea95891562dff3990a58da22662b680fd1e63
atjiao_yuan@yeah.net <atjiao_yuan@yeah.net>
2021-08-25 14:14:28 +0800

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-08-20 10:52:16
+ * @LastEditTime: 2021-08-20 10:54:06
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue3-ts-demo\src\views\About.vue
+-->
+<template>
+  <div class="about">
+    <h1>This is an about page--</h1>
+  </div>
+</template>
